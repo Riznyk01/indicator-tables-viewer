27635ae7777f59c663526492b1871ca92165fd10
Oleksandr Riznyk <OleksandrRiznyk01@gmail.com>
2024-03-29 00:12:28 +0200

    Moved UI code to ui package

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
 	_ "github.com/nakagami/firebirdsql"
 	"indicator-tables-viewer/internal/config"
 	"indicator-tables-viewer/internal/repository"
+	"indicator-tables-viewer/internal/ui"
 	"log"
 )
 
 func main() {
-
 	a := app.New()
-	window := a.NewWindow("Dropdown Example")
-	window.Resize(fyne.NewSize(1000, 600))
+	//a.SetIcon(resourceIconPng)
 
 	cfg := config.NewConfig()
 	db, err := repository.NewFirebirdDB(cfg)
@@ -23,20 +19,8 @@ func main() {
 		log.Println(err)
 	}
 	repo := repository.NewRepository(db)
-	tablesList, _ := repo.GetTable()
 
-	dropdown := widget.NewSelect(tablesList, func(selected string) {
-		tableName := selected[:7]
-		header, _ := repo.Viewing.GetHeader(tableName)
-		indicatorNumbers, _ := repo.Viewing.GetIndicatorMaket(tableName)
-		log.Printf("tmp message table header fetched: %s\n", header)
-		log.Printf("tmp message fetched the indicators' numbers: %s\n", indicatorNumbers)
-	})
-	content := container.NewVBox(
-		widget.NewLabel("Select a table:"),
-		dropdown,
-	)
-	window.SetContent(content)
-	window.ShowAndRun()
-
+	v := ui.NewViewer(repo)
+	v.LoadUI(a)
+	//v.Run()
 }
